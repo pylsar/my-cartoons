932673fedcbd0a3f74051513c4281b500a08bb7f
pylsar <kysov2006@bk.ru>
2020-02-22 08:50:16 +0300

    aboutBox

--- src/components/AboutBox/AboutBox.jsx
+++ src/components/AboutBox/AboutBox.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 import './AboutBox.scss'
+import closeIcon from '../../assets/img/close.svg'
 
-const AboutBox = () => {
+
+const AboutBox = ( {title, link} ) => {
+
+   
+
+
     return (
         <div className="aboutBox__more">
-            <h1>gmn</h1>
+            <img src={closeIcon} alt="closeBtn"/>
+            
+            <h2>{title}</h2>
+            {title==="Подробнее" || title==='Премии' ? 'Больше информации можно получить на сайте википедия, нажав на ссылку ниже' : 'Перейдя по ссылке ниже, вы увидите список ток 15 мультфильмов от компании walt disney'}
+            <a href={link} rel="nofollow" target="_blank">тут</a>
         </div>
     )
 }
--- src/components/AboutWaltDisney/AboutWaltDisney.jsx
+++ src/components/AboutWaltDisney/AboutWaltDisney.jsx
@@ -34,21 +34,35 @@ class AboutWaltDisney extends React.Component {
 
     render(){
 
-        const more = this.state.more && <div><AboutBox /></div>
-        const prize = this.state.prize && <div><AboutBox /></div>
-        const top = this.state.top && <div><AboutBox /></div>
+        const more = this.state.more && <div><AboutBox 
+        title="Подробнее"
+        link="https://ru.wikipedia.org/wiki/The_Walt_Disney_Company"
+        />
+        </div>
+        const prize = this.state.prize && <div><AboutBox 
+        title="Премии"
+        link="https://ru.wikipedia.org/wiki/Список_наград_и_номинаций_Уолта_Диснея_на_премию_«Оскар»"
+        />
+        </div>
+        const top = this.state.top && <div><AboutBox 
+        title="Топ"
+        link="https://way2day.com/article/multfilmy-disnej-top-15-luchshih-multikov.html"
+        />
+        </div>
 
         return (
             <div className="aboutBox">
-                <div className="aboutBox__header">
-                <h1>Walt Disney</h1>
-                </div>  
-                <p>The Walt Disney Company — один из крупнейших конгломератов индустрии развлечений в мире. Основанная 16 октября 1923 года братьями Уолтером и Роем Диснеями как небольшая анимационная студия, в настоящее время является одной из крупнейших голливудских студий, владельцем 11 парков развлечений и двух аквапарков, а также нескольких сетей телерадиовещания, к числу которых относится американская телерадиовещательная компания (Эй-Би-Си).</p>
-                <p>Штаб-квартира компании Уолта Диснея и основные производственные мощности сосредоточены в подразделении Disney Studios (студия Уолта Диснея) в городе Бербанк, штат Калифорния, США.</p>
-                <p>Компания Уолта Диснея входит в промышленный индекс Доу-Джонса. Рыночная капитализация на ноябрь 2019 года — 240 млрд долларов.</p>
-                {more}
-                {prize}
-                {top}
+                <div>
+                    <div className="aboutBox__header">
+                    <h1>Walt Disney</h1>
+                    </div>  
+                    <p>The Walt Disney Company — один из крупнейших конгломератов индустрии развлечений в мире. Основанная 16 октября 1923 года братьями Уолтером и Роем Диснеями как небольшая анимационная студия, в настоящее время является одной из крупнейших голливудских студий, владельцем 11 парков развлечений и двух аквапарков, а также нескольких сетей телерадиовещания, к числу которых относится американская телерадиовещательная компания (Эй-Би-Си).</p>
+                    <p>Штаб-квартира компании Уолта Диснея и основные производственные мощности сосредоточены в подразделении Disney Studios (студия Уолта Диснея) в городе Бербанк, штат Калифорния, США.</p>
+                    <p>Компания Уолта Диснея входит в промышленный индекс Доу-Джонса. Рыночная капитализация на ноябрь 2019 года — 240 млрд долларов.</p>
+                    {more}
+                    {prize}
+                    {top}
+                </div>
                 <div className="aboutBox__buttons">
                     <Button onClick={this.moreClick} buttonName="Подробнее"/>
                     <Button onClick={this.prizeClick} buttonName="Премии"/>
