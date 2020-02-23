b196431c221a2d9008542a8aa319fb1907c71d3d
pylsar <kysov2006@bk.ru>
2020-02-23 09:49:35 +0300

    styles AboutBox

--- src/components/AboutBox/AboutBox.jsx
+++ src/components/AboutBox/AboutBox.jsx
@@ -17,8 +17,8 @@ const AboutBox = ( {title, link} ) => {
             <img onClick={ () => setIsModalOpen(false) } src={closeIcon} alt="closeBtn"/>
             
             <h2>{title}</h2>
-            {title==="Подробнее" || title==='Премии' ? 'Больше информации можно получить на сайте википедия, нажав на ссылку ниже' : 'Перейдя по ссылке ниже, вы увидите список ток 15 мультфильмов от компании walt disney'}
-            <a className="aboutBox__more--link" href={link} rel="nofollow" target="_blank">клик</a>
+            {title==="Подробнее" || title==='Премии' ? <p>Больше информации можно получить на сайте википедия, нажав на ссылку ниже</p> : <p>Перейдя по ссылке ниже, вы увидите список ток 15 мультфильмов от компании walt disney</p>}
+            <a className="aboutBox__more--link" href={link} rel="nofollow" target="_blank"><span>к</span><span>л</span><span>и</span><span>к</span></a>
         </div>
         )}  
         </Fragment>
