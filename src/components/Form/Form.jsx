f4a01d754745e095b3d05062fdaeddd2b59528b8
pylsar <kysov2006@bk.ru>
2020-02-24 13:21:36 +0300

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,24 +6,80 @@ import Button from '../Button/Button'
 import DonaldMail from '../../assets/img/mailman.png'
 import './Form.scss'
 
-const Form = () => {
-    return (
-        <div className="form">
-            <h1>Отправте отзыв</h1>
-            <span className="form--subscription">После отправки отзыва наши почтальоны лично доставят Ваши сообщения до мульт корпораций </span>
-            <div className="form__container">
-                <form className="form__container__box">  
-                    <Input name="Email" id="Email"/>
-                    <Input name="Cartoon" id="Cartoon"/>
-                    <Input name="Age" id="Age"/>
-                    <label htmlFor="text" className="form__container__box--labeltextarea">Type Here</label>
-                    <textarea id="text" rows="8" cols="45" name="text"></textarea>
-                    <Button buttonName="Отправить Дональда"/>
-                </form>
-                <img src={DonaldMail} alt="почтальон Дональд"/>
-            </div>    
-        </div>
-    )
+class Form extends React.Component {
+
+    state = {
+        Email: '',
+        Password: '',
+        Age: '',
+        text: ''
+    }
+
+    handlerSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    // handleClick = () => {
+    //     this.setState({
+    //         value: null
+    //     })
+    // }
+
+
+    render(){
+        return (
+            <div className="form">
+                <h1>Отправьте отзыв</h1>
+                <span className="form--subscription">После отправки отзыва наши почтальоны лично доставят Ваши сообщения до мульт корпораций </span>
+                <div className="form__container">
+                    <form className="form__container__box" onSubmit={this.handlerSubmit}>  
+                        <Input 
+                            onChange={this.handleChange} 
+                            name="Email" 
+                            id="Email" 
+                            value={this.state.value}
+                            required
+                        />
+                        <Input 
+                            onChange={this.handleChange} 
+                            name="Cartoon" 
+                            id="Cartoon" 
+                            value={this.state.value}
+                            required
+                        />
+                        <Input 
+                            onChange={this.handleChange} 
+                            name="Age" 
+                            id="Age" 
+                            type="number"
+                            value={this.state.value}
+                        />
+                        <label htmlFor="text" className="form__container__box--labeltextarea">Type Here</label>
+                        <textarea 
+                            id="text" 
+                            rows="8" 
+                            cols="45" 
+                            name="text"
+                            onChange={this.handleChange}
+                            value={this.state.value}
+                        />
+                        <Button 
+                            buttonName="Отправить Дональда"
+                            // onClick={this.handleClick}
+                            />
+                    </form>
+                    <img src={DonaldMail} alt="почтальон Дональд"/>
+                </div>    
+            </div>
+        )
+    }
 }
 
 export default Form
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import './Input.scss'
 
-const Input = ({type, value, name, id}) => {
+const Input = ({type, value, name, id, onChange}) => {
     return (
         <div className="input">
             <label htmlFor={id}>{name}</label>
-            <input type={type} value={value} name={name} id={name}/>
+            <input type={type} value={value} name={name} id={name} onChange={onChange}/>
         </div>
     )
 }
