78554a8f94be7adc50b110842c92dcd7e2a9aa59
pylsar <kysov2006@bk.ru>
2020-03-05 07:32:27 +0300

    about marvel

--- src/components/AboutMarvel/AboutMarvel.jsx
+++ src/components/AboutMarvel/AboutMarvel.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import './AboutMarvel.scss'
+
 const AboutMarvel = () => {
     return (
-        <div>
-            Marvel
-        </div>
+        <div className="marvel">
+            <div className="marvel__box">
+                
+            </div>
+                <div className="marvel__left">
+                123
+                </div>
+                <div className="marvel__right">
+                sdf
+                </div>
+                <div className="marvel__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rerum maiores quaerat enim cum cumque ab aliquid iste iure. Similique quis molestias reiciendis quisquam magnam ea, voluptatibus repudiandae ipsa dolores.
+                Molestiae voluptatem saepe inventore incidunt quae, dignissimos dolores nulla in perferendis pariatur impedit odio fugit dolor nemo quis laborum, soluta illo consectetur aliquid atque error. Pariatur voluptatem facere itaque at.
+                Ut eaque nihil nisi, dolor quis ullam est dignissimos laudantium cum facilis officiis, vel nostrum temporibus. Officia nulla corrupti corporis pariatur suscipit dolor, maxime debitis, eaque voluptate fugit soluta et!
+                </div>
+        </div>  
     )
 }
 
